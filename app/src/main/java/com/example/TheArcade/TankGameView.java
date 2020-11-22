f2b48c8c25e40933131a151aada1718489d84cc9
AlyssaChow <alyssachow00@gmail.com>
2020-11-22 13:29:25 -0500

    Almost done :)

--- app/src/main/java/com/example/TheArcade/TankGameView.java
+++ app/src/main/java/com/example/TheArcade/TankGameView.java
@@ -5,14 +5,25 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Region;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class TankGameView extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
 
-    //private Canvas canvas;
+    private int currentMap = 0;
+    private int[] maps = {
+            R.raw.map1,
+            R.raw.map2,
+            R.raw.map3,
+            R.raw.map4,
+            R.raw.map5
+    };
 
     public TankGameView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
@@ -37,28 +48,15 @@ public class TankGameView extends SurfaceView implements SurfaceHolder.Callback 
 
         Map.start(getResources());
 
-        Bitmap tankBmp = BitmapFactory.decodeResource(getResources(), R.drawable.tank);
-        Bitmap barrelBmp = BitmapFactory.decodeResource(getResources(), R.drawable.barrel);
 
-        Bitmap eTankBmp = BitmapFactory.decodeResource(getResources(), R.drawable.enemytank);
-        Bitmap eBarrelBmp = BitmapFactory.decodeResource(getResources(), R.drawable.enemybarrel);
+        Data.tankBmp = BitmapFactory.decodeResource(getResources(), R.drawable.tank);
+        Data.barrelBmp = BitmapFactory.decodeResource(getResources(), R.drawable.barrel);
+        Data.eTankBmp = BitmapFactory.decodeResource(getResources(), R.drawable.enemytank);
+        Data.eBarrelBmp = BitmapFactory.decodeResource(getResources(), R.drawable.enemybarrel);
 
-        Data.get().tank = new Tank(tankBmp, barrelBmp);
-        Data.get().tank.xPos = 400;
-        Data.get().tank.yPos = 600;
-        Data.get().tank.playable = true;
-
-        Data.get().enemyTank = new Tank(eTankBmp, eBarrelBmp);
-        Data.get().enemyTank.xPos = 3000;
-        Data.get().enemyTank.yPos = 600;
-        Data.get().enemyTank.rotation = 90;
-
-        Data.get().map = new Map(R.raw.map1);
-
+        //Data.get().map = new Map(maps[currentMap]);
 
         Bullet1.start(BitmapFactory.decodeResource(getResources(), R.drawable.bullet));
-
-
     }
 
     @Override
@@ -75,20 +73,60 @@ public class TankGameView extends SurfaceView implements SurfaceHolder.Callback 
         }
     }
 
+    private double mapTimeout = 0;
 
     public void update(double deltaTime) {
-        Data.get().tank.update(deltaTime);
-        Data.get().enemyTank.update(deltaTime);
-        Bullet1.update(deltaTime);
+        if (Data.get().map != null) {
+            mapTimeout = 1.5;
 
-        Spritetank.updateSprites(deltaTime);
+            Log.d("DEBUG: ", "in tgv update, map != null");
+
+            Data.get().tank.update(deltaTime);
+            if (!Data.get().tank.alive) {
+                // Player lost
+                Data.get().map.destroy();
+                Data.get().map = null;
+                return;
+            }
+
+            int numEnemies = Data.get().enemyTanks.size();
+
+            for (int i = 0; i < Data.get().enemyTanks.size(); i++) {
+                Data.get().enemyTanks.get(i).update(deltaTime);
+                if (!Data.get().enemyTanks.get(i).alive)
+                    numEnemies--;
+            }
+
+            if (numEnemies == 0) {
+                // Player won
+                currentMap++;
+                Data.get().map.destroy();
+                Data.get().map = null;
+                return;
+            }
+
+            Bullet1.update(deltaTime);
+            Spritetank.updateSprites(deltaTime);
+        } else {
+            if (currentMap == maps.length) {
+                //WINNER
+                return;
+            }
+            Log.d("DEBUG: ", "in tgv update, map == null");
+            if (mapTimeout <= 0) {
+                Data.get().map = new Map(maps[currentMap]);
+            } else {
+                mapTimeout -= deltaTime;
+            }
+
+        }
+
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        if (canvas != null) {
-
+        if (canvas != null && Data.get().map != null) {
             // Camera
             float yScale = (float)canvas.getHeight() / Data.get().map.getHeight();
             float xScale = (float)canvas.getWidth() / Data.get().map.getWidth();
@@ -101,6 +139,10 @@ public class TankGameView extends SurfaceView implements SurfaceHolder.Callback 
 
             canvas.drawColor(Color.LTGRAY);
             Data.get().map.draw(canvas);
+
+            Data.get().tank.draw(canvas);
+            for (int i = 0; i < Data.get().enemyTanks.size(); i++)
+                Data.get().enemyTanks.get(i).draw(canvas);
 
             Spritetank.drawSprites(canvas);
         }
